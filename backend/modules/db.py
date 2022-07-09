8268dfb2170935fb8910665134e99cbaa14ec8ff
Marcos Vinícius Queiroz <49909597+Kieroth29@users.noreply.github.com>
2022-07-09 19:21:43 -0300

    DB user and password as environment variables

--- backend/modules/db.py
+++ backend/modules/db.py
@@ -3,8 +3,7 @@ import psycopg2 as pg
 
 class DB():
     def __init__(self):
-        #self.conn = pg.connect(database=environ.get('DB_NAME'), user=environ.get('DB_USER'), password=environ.get('DB_PASSWORD'))
-        self.conn = pg.connect(database='task-list', user='postgres', password='pgsql@kieroth29')
+        self.conn = pg.connect(database='task-list', user=environ.get('DB_USER'), password=environ.get('DB_PASSWORD'))
         self.cursor = self.conn.cursor()
         
     def commit(self):
